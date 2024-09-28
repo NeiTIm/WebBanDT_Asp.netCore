092ed8d441f1ae6b496ff7bb466dd498a5747845
NeiTIm <tien.dmxth@gamil.com>
2024-09-28 17:23:28 +0700

    update lan2 trang admin

--- WebBanDT/Areas/Admin/Controllers/ProductController.cs
+++ WebBanDT/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebBanDT.Models.Repository;
 
 namespace WebBanDT.Areas.Admin.Controllers
@@ -11,9 +12,9 @@ namespace WebBanDT.Areas.Admin.Controllers
         {
             _dataContext = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _dataContext.Products.OrderByDescending(p=>p.Id).Include(p=>p.Category).Include(p=>p.Brand).ToListAsync());
         }
     }
 }
